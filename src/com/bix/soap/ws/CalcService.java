2b26fbd52bc77ceee778ff9b673ea225175e7642
Pc <Pc@DESKTOP-5DSDVDB.Dlink>
2017-07-09 20:10:15 +0530

    Axis2 Webservice SOAP Example

---
+++ src/com/bix/soap/ws/CalcService.java
@@ -0,0 +1,13 @@
+package com.bix.soap.ws;
+public class CalcService {
+	
+	
+	public int add(int num1,int num2)
+	
+	{ System.out.println("in add method"); return num1+num2;}
+	
+	public int sub(int num1,int num2)
+	{ System.out.println("in sub method"); return num1-num2;}
+	
+	
+}
